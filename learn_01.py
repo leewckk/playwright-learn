e577dbdc607e584f1dbd465e62fc5d3f1dc401de
liwenchao <liwenchao@reach.store>
2024-02-02 10:16:18 +0800

    init commit

---
+++ learn_01.py
@@ -0,0 +1,14 @@
+import logging
+
+from playwright.sync_api import sync_playwright
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logging.fatal(e)
